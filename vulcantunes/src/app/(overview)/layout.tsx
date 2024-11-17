d17efad14a67bcded5b8cf344e25d1a031b537f7
lucas <lucaseschultz@gmail.com>
2024-11-17 09:18:40 -0700

    Cleaned up code

--- vulcantunes/src/app/(overview)/layout.tsx
+++ vulcantunes/src/app/(overview)/layout.tsx
@@ -2,18 +2,17 @@ import TopNav from "@/app/ui/overview/topnav";
 import Footer from "@/app/ui/overview/footer";
 
 export default function Layout({
-                                     children,
-                                   }: Readonly<{
+  children
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <div className={"page"}>
       <header>
         <TopNav></TopNav>
       </header>
       {children}
-      <Footer></Footer>
+      <Footer />
     </div>
   );
-};
-
+}
