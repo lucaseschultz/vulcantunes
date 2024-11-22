8097500fb8b415f745dec8acf0ccf35656769752
lucas <lucaseschultz@gmail.com>
2024-11-22 15:40:40 -0700

    Moved footer.tsx styling to overview.css

--- vulcantunes/src/app/ui/overview/footer.tsx
+++ vulcantunes/src/app/ui/overview/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
-      <div>
+      <div className="copyright">
         Copyright © {currentYear}
         <a href="https://www.vulcantunes.com"> VulcanTunes.com</a>
       </div>
