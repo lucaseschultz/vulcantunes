580e6888e51ff83c0a091760cd281964e40e9bc4
lucas <lucaseschultz@gmail.com>
2024-11-22 15:26:48 -0700

    Made topnav.tsx

---
+++ vulcantunes/src/app/ui/overview/topnav.tsx
@@ -0,0 +1,14 @@
+export default function TopNav() {
+    return (
+      <nav className={'top-nav'}>
+        <ul className={'top-nav-left'}>
+          <li>test1</li>
+          <li>test2</li>
+        </ul>
+        <ul className={'top-nav-right'}>
+          <li>right-test1</li>
+          <li>right-test2</li>
+        </ul>
+      </nav>
+    );
+};
