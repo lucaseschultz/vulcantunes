77eac2e5c97d13766312072d4becf080aa061893
lucas <lucaseschultz@gmail.com>
2024-12-16 17:50:26 -0700

    Moved potential header image's brightness styling from overview.css to header-image.tsx & added style for dynamic rendering

--- vulcantunes/src/app/ui/overview/header-image.tsx
+++ vulcantunes/src/app/ui/overview/header-image.tsx
@@ -2,15 +2,18 @@ import Image from "next/image";
 
 export default function HeaderImage() {
     return (
-      <div>
-        <Image
-            src="/vulcantunes-header-image-with-logo.png"
-            alt="Vulcantunes.com logo - Your ride just got better"
-            width={1080}
-            height={674}
-            priority
-        />
-      </div>
+      <Image
+          src="/vulcantunes-header-image-cropped.png"
+          alt="Vulcantunes.com logo - Your ride just got better"
+          width={1080}
+          height={674}
+          style={{
+            width: '100vw',
+            height: 'auto',
+            // filter: 'brightness(65%)',
+          }}
+          priority
+      />
     );
 }
 
