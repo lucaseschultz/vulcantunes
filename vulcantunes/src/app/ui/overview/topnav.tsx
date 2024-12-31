78a880be284022ad9e81602b00706dce41b6636d
lucas <lucaseschultz@gmail.com>
2024-12-31 11:03:09 -0700

    Increased size of icons, when using desktop; Removed task from todo list

--- vulcantunes/src/app/ui/overview/topnav.tsx
+++ vulcantunes/src/app/ui/overview/topnav.tsx
@@ -3,46 +3,50 @@
 import type { ImageNavItem, IconNavItem } from '@/app/lib/definitions';
 import { MagnifyingGlass, ShoppingCart, User, Heart } from "@phosphor-icons/react/dist/ssr"
 import { DisplayNavItems } from '@/app/lib/utils';
+import { useEffect, useState } from 'react';
 
-const ICON_SIZE = 24;
-const TopNavLeft: ImageNavItem[] = [
-    {
+const getIconSize = () => window.innerWidth > 768 ? 28 : 24;
+
+export default function TopNav() {
+  const [iconSize, setIconSize] = useState(getIconSize());
+
+  useEffect(() => {
+    const handleResize = () => setIconSize(getIconSize());
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  const TopNavLeft: ImageNavItem[] = [{
     name: 'Home Page Link',
     href: '/',
     icon: "/vulcantunes-icon.png",
     alt: 'VulcanTunes Icon',
-    width: ICON_SIZE,
-    height: ICON_SIZE,
-    }
-];
-const TopNavRight: IconNavItem[] = [
-  {
+    width: iconSize,
+    height: iconSize,
+  }];
+
+  const TopNavRight: IconNavItem[] = [{
     name: 'Search',
     href: '/search',
     icon: MagnifyingGlass,
-    size: ICON_SIZE,
-  },
-  {
+    size: iconSize,
+  }, {
     name: 'Cart',
     href: '/cart',
     icon: ShoppingCart,
-    size: ICON_SIZE,
-  },
-  {
+    size: iconSize,
+  }, {
     name: 'Wishlist',
     href: '/wishlist',
     icon: Heart,
-    size: ICON_SIZE,
-  },
-  {
+    size: iconSize,
+  }, {
     name: 'Account',
     href: '/account',
     icon: User,
-    size: ICON_SIZE,
-  },
-];
+    size: iconSize,
+  }];
 
-export default function TopNav() {
   return (
     <nav className='top-nav' aria-label='Main navigation'>
       <DisplayNavItems NavName='top-nav-left' NavItems={TopNavLeft} />
