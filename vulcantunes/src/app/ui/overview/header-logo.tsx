3dab9d34969f7053fb0baaeac6218c6ae52c398b
lucas <lucaseschultz@gmail.com>
2025-01-08 14:51:52 -0700

    Removed unnecessary div wrapping HeaderLogo

--- vulcantunes/src/app/ui/overview/header-logo.tsx
+++ vulcantunes/src/app/ui/overview/header-logo.tsx
@@ -2,21 +2,19 @@ import Image from "next/image";
 
 export default function HeaderLogo() {
   return (
-    <div className="header">
-      <Image
-        src="/vulcantunes-logo.png"
-        alt="Vulcantunes.com logo - Your ride just got better"
-        width={491}
-        height={101}
-        className={'header-logo'}
-        style={{
-          width: '40lvw',
-          height: 'auto',
-          zIndex: 1,
-        }}
-        priority
-      />
-    </div>
+    <Image
+      src="/vulcantunes-logo.png"
+      alt="Vulcantunes.com logo - Your ride just got better"
+      width={491}
+      height={101}
+      className={'header-logo'}
+      style={{
+        width: '40lvw',
+        height: 'auto',
+        zIndex: 1,
+      }}
+      priority
+    />
   );
 }
 
