bd29961aeaada7138eb748852e9a50f2c411d7e8
lucas <lucaseschultz@gmail.com>
2025-01-08 07:14:11 -0700

    Changed pages function name from "Home" to "Page"

--- vulcantunes/src/app/(overview)/page.tsx
+++ vulcantunes/src/app/(overview)/page.tsx
@@ -3,7 +3,7 @@ import HeaderImage from "@/app/ui/overview/header-image";
 import HeaderLogo from "@/app/ui/overview/header-logo";
 import Products from "@/app/ui/overview/products"
 
-export default function Home() {
+export default function Page() {
   return (
     <main>
       <header>
