b7c654200ce0087b895efa5901378604902a50f1
lucas <lucaseschultz@gmail.com>
2025-01-09 13:21:41 -0700

    Moved fetch window size function to utils.tsx

--- vulcantunes/src/app/lib/utils.tsx
+++ vulcantunes/src/app/lib/utils.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from 'next/navigation';
@@ -37,4 +38,31 @@ export function DisplayNavItems({ NavName, NavItems }: { NavName: string, NavIte
       })}
     </ul>
   );
+}
+
+interface WindowSize {
+  width: number
+  height: number
+}
+export function fetchWindowSize() {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    height: typeof window !== 'undefined' ? window.innerHeight : 0,
+  })
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return windowSize
 }
