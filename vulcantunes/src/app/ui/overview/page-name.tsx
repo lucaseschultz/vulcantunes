c4312db0ccd12f83d638b7185266522df28283cb
lucas <lucaseschultz@gmail.com>
2025-01-18 13:16:36 -0700

    Created dynamically rendering title

--- vulcantunes/src/app/ui/overview/page-name.tsx
+++ vulcantunes/src/app/ui/overview/page-name.tsx
@@ -1,5 +1,13 @@
+'use client'
+
+import { usePathname } from 'next/navigation';
+
 export default function PageName() {
+  const pathname = usePathname();
+  const lastSegment = pathname.split('/').pop() || 'Products';
+  const formattedSegment = lastSegment.charAt(0).toUpperCase() + lastSegment.slice(1);
+
   return (
-  <div></div>
+    <h1 className="page-title">{formattedSegment}</h1>
   );
 }
