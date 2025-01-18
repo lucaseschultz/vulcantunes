76538049e70bc5bdee0395c5889ce3458fba258a
lucas <lucaseschultz@gmail.com>
2025-01-18 12:44:55 -0700

    Added more template countries to check scrolling/padding

--- vulcantunes/src/app/ui/overview/products.tsx
+++ vulcantunes/src/app/ui/overview/products.tsx
@@ -9,11 +9,16 @@ interface Country {
   id: string
 }
 const COUNTRIES: readonly Country[] = [
+  { name: "United States", continent: "North America", id: "us" },
+  { name: "Australia", continent: "Oceania", id: "au" },
   { name: "Belgium", continent: "Europe", id: "be" },
   { name: "Bolivia", continent: "South America", id: "bo" },
+  { name: "Brazil", continent: "South America", id: "br" },
+  { name: "Canada", continent: "North America", id: "ca" },
   { name: "Ghana", continent: "Africa", id: "gh" },
   { name: "India", continent: "Asia", id: "in" },
   { name: "Japan", continent: "Asia", id: "jp" },
+  { name: "Sweden", continent: "Europe", id: "se" }
 ] as const
 const ProductsList = memo(function ProductsList({ countries }: { countries: readonly Country[] }) {
   return (
