d72981afca49be6528a70b31afc4a29efc81987d
lucas <lucaseschultz@gmail.com>
2025-01-23 14:37:30 -0700

    Moved DisplayNavItems from utils.tsx to actions.tsx

--- vulcantunes/src/app/lib/utils.tsx
+++ vulcantunes/src/app/lib/utils.tsx
@@ -1,49 +1,6 @@
 import { useState, useEffect } from 'react'
-import Link from "next/link";
-import Image from "next/image";
-import { usePathname } from 'next/navigation';
-import type { IconNavItem, ImageNavItem } from '@/app/lib/definitions';
+import type { WindowSize } from './definitions';
 
-export function DisplayNavItems({ NavName, NavItems }: { NavName: string, NavItems: (IconNavItem|ImageNavItem)[] }) {
-  const pathname = usePathname();
-
-  return (
-    <ul className={NavName}>
-      {NavItems.map((NavItem) => {
-        const isImage = typeof NavItem.icon === 'string';
-        return (
-          <li key={NavItem.name}>
-            <Link
-              href={NavItem.href}
-              aria-label={`${NavItem.name} button`}
-              title={NavItem.name}
-            >
-              {isImage ? (
-                <Image
-                  src={NavItem.icon as string}
-                  alt={isImage ? (NavItem as ImageNavItem).alt : NavItem.name}
-                  width={(NavItem as ImageNavItem).width}
-                  height={(NavItem as ImageNavItem).height}
-                />
-              ) : (
-                <NavItem.icon
-                  size={(NavItem as IconNavItem).size}
-                  aria-hidden="true"
-                  weight={NavItem.href === pathname ? 'fill' : 'regular'}
-                />
-              )}
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-interface WindowSize {
-  width: number
-  height: number
-}
 export function fetchWindowSize() {
   const [windowSize, setWindowSize] = useState<WindowSize>({
     width: 0,
