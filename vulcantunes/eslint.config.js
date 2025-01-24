4208357902fb3705a10fe73bbe26dfd21e012700
lucas <lucaseschultz@gmail.com>
2025-01-24 14:33:11 -0700

    Adding new eslint config to git

---
+++ vulcantunes/eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import nextPlugin from '@next/eslint-plugin-next'
+import typescript from '@typescript-eslint/eslint-plugin'
+import typescriptParser from '@typescript-eslint/parser'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      '@next/next': nextPlugin,
+      '@typescript-eslint': typescript
+    },
+    rules: {
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error']
+    }
+  }
+]
