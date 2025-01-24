da830f99445161b67f0005281bef9466a7418c72
lucas <lucaseschultz@gmail.com>
2025-01-24 10:57:54 -0700

    Removed vercel auth

--- vulcantunes/src/app/lib/actions.tsx
+++ vulcantunes/src/app/lib/actions.tsx
@@ -1,7 +1,5 @@
 'use server';
 
-import { signIn } from '@/auth';
-import { AuthError } from 'next-auth';
 import { usePathname } from 'next/navigation';
 import type { IconNavItem, ImageNavItem } from './definitions';
 import Link from "next/link";
@@ -41,23 +39,4 @@ export async function DisplayNavItems({ NavName, NavItems }: { NavName: string, 
         })}
       </ul>
   );
-}
-
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    await signIn('credentials', formData);
-  } catch (error) {
-    if (error instanceof AuthError) {
-      switch (error.type) {
-        case 'CredentialsSignin':
-          return 'Invalid credentials.';
-        default:
-          return 'Something went wrong.';
-      }
-    }
-    throw error;
-  }
 }
