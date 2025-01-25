88a2d2f685f43583ba7247992140d25e51e9b003
lucas <lucaseschultz@gmail.com>
2025-01-25 15:26:41 -0700

    Made config stricter

--- vulcantunes/next.config.ts
+++ vulcantunes/next.config.ts
@@ -4,7 +4,11 @@ const nextConfig = {
   env: {
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-  }
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  reactStrictMode: true
 } satisfies NextConfig;
 
 export default nextConfig;
