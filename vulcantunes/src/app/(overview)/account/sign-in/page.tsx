2cc4ecaad546e137e983e3fa019310400ba9ff6d
lucas <lucaseschultz@gmail.com>
2025-01-25 16:25:07 -0700

    updated sign-in css import

--- vulcantunes/src/app/(overview)/account/sign-in/page.tsx
+++ vulcantunes/src/app/(overview)/account/sign-in/page.tsx
@@ -1,4 +1,4 @@
-import '@/src/app/ui/overview/account/sign-in'
+import '@/src/app/ui/overview/account/sign-in/sign-in.css'
 import SignInForm from "@/src/app/ui/overview/account/sign-in/sign-in-form"
 
 export default function Page() {
