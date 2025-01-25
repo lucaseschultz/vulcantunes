017c24531b63706031879a340185c546a2b528ab
lucas <lucaseschultz@gmail.com>
2025-01-25 16:26:23 -0700

    Updated route.ts to work with https/run build

--- vulcantunes/src/app/api/auth[...nextauth]/route.ts
+++ vulcantunes/src/app/api/auth[...nextauth]/route.ts
@@ -2,4 +2,11 @@ import NextAuth from 'next-auth'
 import { authOptions } from '@/src/app/lib/auth'
 
 const handler = NextAuth(authOptions)
-export { handler as GET, handler as POST }
+
+export function GET() {
+  return Response.json(handler)
+}
+
+export function POST() {
+  return Response.json(handler)
+}
