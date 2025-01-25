4f35b24a4e230c54367f803960fcfaf08ba2f9bf
lucas <lucaseschultz@gmail.com>
2025-01-25 12:05:08 -0700

    Created template sign-in page/component

---
+++ vulcantunes/src/app/(overview)/account/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import '@/src/app/ui/overview/account/sign-in'
+import SignInForm from "@/src/app/ui/overview/account/sign-in/sign-in-form"
+
+export default function Page() {
+  return (
+    <SignInForm />
+  );
+};
