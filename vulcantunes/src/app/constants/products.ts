dfa42d2b308e67e040b560934170ca2ae9542f9e
lucas <lucaseschultz@gmail.com>
2025-01-25 14:18:41 -0700

    Moved constants to their own file

---
+++ vulcantunes/src/app/constants/products.ts
@@ -0,0 +1,22 @@
+import { Product } from "@/src/app/lib/definitions";
+
+export const PRODUCTS = [
+  { name: "United States", continent: "North America", id: "us" },
+  { name: "Australia", continent: "Oceania", id: "au" },
+  { name: "Belgium", continent: "Europe", id: "be" },
+  { name: "Bolivia", continent: "South America", id: "bo" },
+  { name: "Brazil", continent: "South America", id: "br" },
+  { name: "Canada", continent: "North America", id: "ca" },
+  { name: "Ghana", continent: "Africa", id: "gh" },
+  { name: "India", continent: "Asia", id: "in" },
+  { name: "Japan", continent: "Asia", id: "jp" },
+  { name: "Sweden", continent: "Europe", id: "se" }
+] as const satisfies readonly Product[]
+
+export const FEATURE_FILTERS = {
+  "Bluetooth": false,
+  "USB Charger": false,
+  "AUX Input": false,
+  "AUX Output": false,
+  "Audio Jack": false
+} as const
