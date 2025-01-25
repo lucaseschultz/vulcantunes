5ccf8dde26ca74c74c29520e23576724f8139e8b
lucas <lucaseschultz@gmail.com>
2025-01-25 16:24:25 -0700

    Added https/more secure code for auth.ts

--- vulcantunes/src/app/lib/auth.ts
+++ vulcantunes/src/app/lib/auth.ts
@@ -5,5 +5,30 @@ export const authOptions = {
   pages: {
     signIn: '/account/login',
   },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user
+      const isProtectedRoute = nextUrl.pathname.startsWith('/account') ||
+        nextUrl.pathname.startsWith('/admin')
+
+      if (isProtectedRoute) {
+        return isLoggedIn
+      }
+      return true
+    }
+  },
   secret: process.env.NEXTAUTH_SECRET,
+  debug: process.env.NODE_ENV === 'development',
+  trustHost: true,
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true
+      }
+    }
+  }
 } satisfies NextAuthConfig
