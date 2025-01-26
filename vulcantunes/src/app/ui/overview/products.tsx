9f390e1cdfd6911a04962d66e4a7c4bd1a7c3b6a
lucas <lucaseschultz@gmail.com>
2025-01-25 17:35:58 -0700

    Moved content to components; Updated imports

--- vulcantunes/src/app/ui/overview/products.tsx
+++ vulcantunes/src/app/ui/overview/products.tsx
@@ -2,47 +2,9 @@
 
 import { useReducer, useCallback, memo, useState, useEffect, type ChangeEvent } from 'react'
 import { productsReducer, initialState } from '@/src/app/reducers/products-reducer'
-import { FEATURE_FILTERS } from '@/src/app/constants/products'
-import type { Product } from '@/src/app/lib/definitions';
-import { useDebounce } from '@/src/app/hooks/use-debounce'
-import {ProductsErrorBoundary} from "@/src/app/ui/overview/components/products-error-boundary";
-
-const ProductsList = memo(function ProductsList({ products }: { products: readonly Product[] }) {
-  return (
-    <div className="products-list" aria-label="Products list">
-      {products.map(({name, continent, id}) => (
-        <div key={id} className="product-item">
-          <span className="product-name">{name}</span>
-          <span className="product-continent">{continent}</span>
-        </div>
-      ))}
-    </div>
-  )
-})
-
-const FeatureFilters = memo(function FeatureFilters({
-  selectedFeatures,
-  onFeatureChange
-}: {
-  selectedFeatures: Set<string>
-  onFeatureChange: (feature: string) => void
-}) {
-  return (
-    <div className="checkbox-group">
-      {Object.keys(FEATURE_FILTERS).map((feature) => (
-        <label key={feature} className="checkbox-label">
-          <input
-            type="checkbox"
-            className="checkbox-input"
-            checked={selectedFeatures.has(feature)}
-            onChange={() => onFeatureChange(feature)}
-          />
-          {feature}
-        </label>
-      ))}
-    </div>
-  )
-})
+import { ProductsErrorBoundary } from './components/products-error-boundary'
+import { FilterSection } from './components/filter-section'
+import { ProductsList } from './components/products-list'
 
 export default function Products() {
   const [state, dispatch] = useReducer(productsReducer, initialState)
