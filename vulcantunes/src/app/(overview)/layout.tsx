eeaca5cb5dc91e5c2bc73c1b108852be6eaa3190
lucas <lucaseschultz@gmail.com>
2025-01-31 15:16:28 -0700

    Updated imports, due to having moved layout components outside of /overview

--- vulcantunes/src/app/(overview)/layout.tsx
+++ vulcantunes/src/app/(overview)/layout.tsx
@@ -1,7 +1,7 @@
 import '@/src/app/ui/overview/overview-layout.css';
-import TopNav from "@/src/app/ui/overview/components/topnav";
-import HeaderLogo from "@/src/app/ui/overview/components/header-logo";
-import Footer from "@/src/app/ui/overview/components/footer";
+import TopNav from "@/src/app/ui/overview/topnav";
+import HeaderLogo from "@/src/app/ui/overview/header-logo";
+import Footer from "@/src/app/ui/overview/footer";
 
 export default function Layout({
   children
