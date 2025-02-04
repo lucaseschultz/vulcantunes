f9840695db3c2431a08322d9a2c7eae418595b1a
lucas <lucaseschultz@gmail.com>
2025-02-04 08:06:20 -0700

    Declared UMD Global variable

--- vulcantunes/src/app/(overview)/layout.tsx
+++ vulcantunes/src/app/(overview)/layout.tsx
@@ -2,6 +2,7 @@ import '@/src/app/ui/overview/layout/overview-layout.css';
 import TopNav from "@/src/app/ui/overview/layout/topnav";
 import HeaderLogo from "@/src/app/ui/overview/layout/header-logo";
 import Footer from "@/src/app/ui/overview/layout/footer";
+import React from "react";
 
 export default function Layout({
   children
