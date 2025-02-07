20eb96a4a609e411ebbebcb90697b591695368ce
lucas <lucaseschultz@gmail.com>
2025-02-07 08:24:31 -0700

    Moved product pool to route.ts, due to mysql needing to use the server; Added keepAlive settings to prevent connection timeouts

--- vulcantunes/src/app/api/products/route.ts
+++ vulcantunes/src/app/api/products/route.ts
@@ -1,5 +1,17 @@
 import { NextResponse } from 'next/server'
-import { PRODUCT_POOL } from "@/src/app/lib/constants";
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
+})
 
 export async function GET() {
   try {
