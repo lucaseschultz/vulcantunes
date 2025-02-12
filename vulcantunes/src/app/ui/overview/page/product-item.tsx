08b49600cd6d0e2c19416a56b2ecf5f5fa60c8b1
lucas <lucaseschultz@gmail.com>
2025-02-12 12:33:20 -0700

    Restructured product div's for improved displaying

--- vulcantunes/src/app/ui/overview/page/product-item.tsx
+++ vulcantunes/src/app/ui/overview/page/product-item.tsx
@@ -11,18 +11,18 @@ export const ProductItem = memo(function ProductItem({ product }: ProductItemPro
       className="product"
       data-testid={`product-${product_id}`}
     >
+      <Image
+        src={imgSrc}
+        alt={`${product_model} image`}
+        width={200}
+        height={200}
+        style={{
+          height: '200',
+          width: 'auto',
+        }}
+        onError={() => setImgSrc('/products/image-coming-soon.jpg')}
+      />
       <div className="product-details">
-        <Image
-          src={imgSrc}
-          alt={`${product_model} image`}
-          width={200}
-          height={200}
-          style={{
-            height: '200',
-            width: 'auto',
-          }}
-          onError={() => setImgSrc('/products/image-coming-soon.jpg')}
-        />
         <span className="product-name">{product_model}</span>
         <span className="product-price">${product_price}</span>
       </div>
--- vulcantunes/src/app/ui/overview/page/skeletons.tsx
+++ vulcantunes/src/app/ui/overview/page/skeletons.tsx
@@ -1,8 +1,8 @@
 export function ProductSkeleton() {
   return (
     <div className="product shimmer">
-      <div className="product-details">
-        <div className="product-skeleton-image" />
+      <div className="product-skeleton-image" />
+      <div className="product-details" >
         <div className="product-skeleton-model" />
         <div className="product-skeleton-price" />
       </div>
