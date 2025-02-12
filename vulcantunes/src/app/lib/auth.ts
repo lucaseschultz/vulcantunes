5cd50a02eae261b4be486252eb587ffcf5636c71
lucas <lucaseschultz@gmail.com>
2025-02-12 14:00:01 -0700

    Removed error causing line

--- vulcantunes/src/app/lib/auth.ts
+++ vulcantunes/src/app/lib/auth.ts
@@ -19,7 +19,6 @@ export const authOptions = {
   },
   secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === 'development',
-  trustHost: true,
   cookies: {
     sessionToken: {
       name: `__Secure-next-auth.session-token`,
