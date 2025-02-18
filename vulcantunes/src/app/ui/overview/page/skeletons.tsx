c524d9c0c5d5cc5e1f538872621547df6af186bc
lucas <lucaseschultz@gmail.com>
2025-02-18 11:16:54 -0700

    Added name and description to product skeleton

--- vulcantunes/src/app/ui/overview/page/skeletons.tsx
+++ vulcantunes/src/app/ui/overview/page/skeletons.tsx
@@ -2,8 +2,10 @@ export function ProductSkeleton() {
   return (
     <div className="product-item shimmer">
       <div className="product-skeleton-image" />
-      <div className="product-details" >
-        <div className="product-skeleton-price" />
+      <div className="product-details">
+        <div className="product-skeleton-name"/>
+        <div className="product-skeleton-description"/>
+        <div className="product-skeleton-price"/>
       </div>
     </div>
   );
