6cf097ada09eddf67a03b930e7b0429b7a8eeb48
lucas <lucaseschultz@gmail.com>
2025-02-18 09:03:21 -0700

    Updated skeletons.tsx

--- vulcantunes/src/app/ui/overview/page/skeletons.tsx
+++ vulcantunes/src/app/ui/overview/page/skeletons.tsx
@@ -1,9 +1,8 @@
 export function ProductSkeleton() {
   return (
-    <div className="product shimmer">
+    <div className="product-item shimmer">
       <div className="product-skeleton-image" />
       <div className="product-details" >
-        <div className="product-skeleton-model" />
         <div className="product-skeleton-price" />
       </div>
     </div>
