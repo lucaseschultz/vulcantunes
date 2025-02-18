3c364ad510757bcd9698d03d5cdfc2acf163c8b5
lucas <lucaseschultz@gmail.com>
2025-02-18 10:49:17 -0700

    Added product_description and product_name to product-item.tsx

--- vulcantunes/src/app/ui/overview/page/product-item.tsx
+++ vulcantunes/src/app/ui/overview/page/product-item.tsx
@@ -3,8 +3,12 @@ import Image from 'next/image'
 import type { ProductItemProps } from '@/src/app/lib/definitions'
 
 export const ProductItem = memo(function ProductItem({ product }: ProductItemProps) {
-  const { product_model, product_image, product_price, product_id } = product
+  const { product_quantity, product_model, product_description, product_name, product_image, product_price } = product
   const [imgSrc, setImgSrc] = useState(`/${product_image}`)
+
+  const truncatedDescription = product_description.length > 100
+    ? `${product_description.slice(0, 100)}...`
+    : product_description
 
   return (
     <div
@@ -24,6 +28,8 @@ export const ProductItem = memo(function ProductItem({ product }: ProductItemPro
         onError={() => setImgSrc('/products/image-coming-soon.jpg')}
       />
       <div className="product-details">
+        <span className="product-name">{product_name}</span>
+        <p className="product-description">{truncatedDescription}</p>
         <span className="product-price">${product_price}</span>
       </div>
     </div>
