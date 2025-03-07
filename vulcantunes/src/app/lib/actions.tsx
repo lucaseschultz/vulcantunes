163ca39dc3a33483f84ea60ce96422177e6574e2
lucas <lucaseschultz@gmail.com>
2025-03-07 16:15:07 -0700

    Added getProduct to actions.tsx

--- vulcantunes/src/app/lib/actions.tsx
+++ vulcantunes/src/app/lib/actions.tsx
@@ -99,3 +99,14 @@ export function renderOptionValues(type: string, values: string[], productModel:
       return null;
   }
 }
+export async function getProduct(model: string) {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/product/${model}`, {
+    next: { revalidate: 60 }
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch product')
+  }
+
+  return res.json()
+}
