5a43a818681f455cf8c36c7c88cd72ba86f249ac
lucas <lucaseschultz@gmail.com>
2025-03-12 15:31:48 -0600

    Removed unneeded reference code

--- vulcantunes/src/app/(overview)/product/[model]/page.tsx
+++ vulcantunes/src/app/(overview)/product/[model]/page.tsx
@@ -1,25 +1,3 @@
-// import DOMPurify from 'isomorphic-dompurify';
-//
-// const SafeHTML = ({ content }) => {
-//   const sanitizedContent = DOMPurify.sanitize(content, {
-//     ALLOWED_TAGS: ['a', 'p', 'br', 'strong', 'em', 'ul', 'li'],
-//     ALLOWED_ATTR: ['href', 'target', 'rel']
-//   });
-//
-//   return <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />;
-// };
-//
-// export default SafeHTML;
-//
-// // Usage example:
-// const ProductDescription = ({ description }) => {
-//   return (
-//     <div className="product-description">
-//       <SafeHTML className="product-description" content={description} />
-//     </div>
-//   );
-// };
-
 import { Suspense } from 'react'
 import Image from 'next/image'
 import '@/src/app/ui/overview/product[model]/product[model].css'
