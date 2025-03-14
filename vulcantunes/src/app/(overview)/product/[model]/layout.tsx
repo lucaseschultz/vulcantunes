12d0c3faeb75640772b4f47b5942108aac1dbbc3
lucas <lucaseschultz@gmail.com>
2025-03-14 10:16:07 -0600

    Used React.use() to unwrap the params Promise in generateMetadate, per Next.js requirements

--- vulcantunes/src/app/(overview)/product/[model]/layout.tsx
+++ vulcantunes/src/app/(overview)/product/[model]/layout.tsx
@@ -3,9 +3,8 @@ import React from "react";
 import { ProductPageProps } from "@/src/app/lib/definitions";
 
 export async function generateMetadata(props: ProductPageProps): Promise<Metadata> {
-  // Promise.resolve = Explicitly treat params as awaitable, satisfying Next.js's requirement without TypeScript errors
-  const resolvedParams = await Promise.resolve(props.params);
-  const model = resolvedParams.model;
+  const params = React.use(props.params);
+  const model = params.model;
 
   return {
     title: model,
