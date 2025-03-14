f68b8c69320536d7668ae1b4f51cd05db52035b1
lucas <lucaseschultz@gmail.com>
2025-03-14 10:16:44 -0600

    Removed error causing unneeded import

--- vulcantunes/src/app/lib/server-actions.tsx
+++ vulcantunes/src/app/lib/server-actions.tsx
@@ -1,5 +1,3 @@
-import 'server-only';
-
 export async function getProduct(model: string) {
   const res = await fetch(`${process.env.NEXTAUTH_URL}/api/product/${model}`, {
     next: { revalidate: 60 }
