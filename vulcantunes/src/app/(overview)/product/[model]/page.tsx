6cbfa91a7c309033c6f0eac9f3424fe84a712e90
lucas <lucaseschultz@gmail.com>
2025-03-14 14:52:35 -0600

    added "error fetching product" handling for /products/[model] page

--- vulcantunes/src/app/(overview)/product/[model]/page.tsx
+++ vulcantunes/src/app/(overview)/product/[model]/page.tsx
@@ -6,15 +6,20 @@ import { ProductSkeleton } from "@/src/app/ui/overview/layout/skeletons"
 import ProductDetails from "@/src/app/ui/overview/product/[model]/product-details"
 
 export default async function Page({ params }: { params: { model: string } }) {
-  const product = await getProduct(params.model)
+  try {
+    const product = await getProduct(params.model)
 
-  if (!product) {
-    notFound()
+    if (!product) {
+      return notFound()
+    }
+
+    return (
+      <Suspense fallback={<ProductSkeleton/>}>
+        <ProductDetails product={product} />
+      </Suspense>
+    )
+  } catch (error) {
+    console.error("Error fetching product:", error)
+    return notFound()
   }
-
-  return (
-    <Suspense fallback={<ProductSkeleton/>}>
-      <ProductDetails product={product} />
-    </Suspense>
-  )
 }
