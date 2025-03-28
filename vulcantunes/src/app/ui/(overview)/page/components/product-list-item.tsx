da31ee0afae21fb9d89165cabde92476a59c463e
lucas <lucaseschultz@gmail.com>
2025-03-28 12:21:44 -0600

    Renamed product-details-button to be product-details-link for clearer naming

--- vulcantunes/src/app/ui/(overview)/page/components/product-list-item.tsx
+++ vulcantunes/src/app/ui/(overview)/page/components/product-list-item.tsx
@@ -59,7 +59,7 @@ export const ProductListItem = memo(function ProductListItem({ product }: Produc
       </div>
       <Link
         href={`${process.env.NEXTAUTH_URL}/product/${product_model}`}
-        className="product-details-button"
+        className="product-details-link"
         aria-label={`See details for ${product_name}`}
       >
         See Details
