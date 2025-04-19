f83f41eb4f6daf7f6abf466a92bb17f496a2cb90
lucas <lucaseschultz@gmail.com>
2025-04-19 13:34:30 -0600

    Fixed whitespace

--- vulcantunes/src/app/(overview)/product/[model]/page.tsx
+++ vulcantunes/src/app/(overview)/product/[model]/page.tsx
@@ -1,15 +1,15 @@
-import { Suspense } from 'react'
+import {Suspense} from 'react'
 import '@/src/app/ui/(overview)/product/[model]/styles/[model].css'
-import { notFound } from 'next/navigation'
-import { getProduct } from "@/src/app/lib/server-actions"
-import { getErrorType, getErrorMessage } from "@/src/app/lib/utils"
-import { ProductSkeleton } from "@/src/app/ui/(overview)/layout/components/skeletons"
+import {notFound} from 'next/navigation'
+import {getProduct} from "@/src/app/lib/server-actions"
+import {getErrorType, getErrorMessage} from "@/src/app/lib/utils"
+import {ProductSkeleton} from "@/src/app/ui/(overview)/layout/components/skeletons"
 import ProductPageItem from "@/src/app/ui/(overview)/product/[model]/components/product-page-item"
 import ProductViewTracker from "@/src/app/ui/(overview)/product/[model]/components/product-view-tracker"
-import { ErrorMessage } from "@/src/app/ui/(overview)/layout/components/error-message"
-import { ProductPageProps } from "@/src/app/lib/definitions";
+import {ErrorMessage} from "@/src/app/ui/(overview)/layout/components/error-message"
+import {ProductPageProps} from "@/src/app/lib/definitions";
 
-export default async function Page({ params }: ProductPageProps) {
+export default async function Page({params}: ProductPageProps) {
   try {
     const resolvedParams = await (params as unknown as Promise<typeof params>);
     const product = await getProduct(resolvedParams.model)
@@ -21,8 +21,8 @@ export default async function Page({ params }: ProductPageProps) {
     return (
       <div className="product-page-container">
         <Suspense fallback={<ProductSkeleton/>}>
-          <ProductViewTracker productId={product.product_id} />
-          <ProductPageItem product={product} />
+          <ProductViewTracker productId={product.product_id}/>
+          <ProductPageItem product={product}/>
         </Suspense>
       </div>
     )
