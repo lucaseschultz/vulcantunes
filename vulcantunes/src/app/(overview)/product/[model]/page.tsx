f5b372c0baa9ba690ecf83cf75cf6a08ef0895a5
lucas <lucaseschultz@gmail.com>
2025-04-19 14:47:52 -0600

    Removed unneeded suspense

--- vulcantunes/src/app/(overview)/product/[model]/page.tsx
+++ vulcantunes/src/app/(overview)/product/[model]/page.tsx
@@ -1,9 +1,7 @@
-import {Suspense} from 'react'
 import '@/src/app/ui/(overview)/product/[model]/styles/[model].css'
 import {notFound} from 'next/navigation'
 import {getProduct} from "@/src/app/lib/server-actions"
 import {getErrorType, getErrorMessage} from "@/src/app/lib/utils"
-import {ProductSkeleton} from "@/src/app/ui/(overview)/layout/components/skeletons"
 import ProductPageItem from "@/src/app/ui/(overview)/product/[model]/components/product-page-item"
 import ProductViewTracker from "@/src/app/ui/(overview)/product/[model]/components/product-view-tracker"
 import {ErrorMessage} from "@/src/app/ui/(overview)/layout/components/error-message"
@@ -20,10 +18,8 @@ export default async function Page({params}: ProductPageProps) {
 
     return (
       <div className="product-page-container">
-        <Suspense fallback={<ProductSkeleton/>}>
-          <ProductViewTracker productId={product.product_id}/>
-          <ProductPageItem product={product}/>
-        </Suspense>
+        <ProductViewTracker productId={product.product_id}/>
+        <ProductPageItem product={product}/>
       </div>
     )
   } catch (error: any) {
