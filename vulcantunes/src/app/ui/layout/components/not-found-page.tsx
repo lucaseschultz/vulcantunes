10e6004bbd8a2f5e072f65eaad31935e80d2d800
lucas <lucaseschultz@gmail.com>
2025-04-29 14:48:33 -0600

    Made code more readable

--- vulcantunes/src/app/ui/layout/components/not-found-page.tsx
+++ vulcantunes/src/app/ui/layout/components/not-found-page.tsx
@@ -10,6 +10,7 @@ export default function NotFoundPage() {
       <HeaderLogo/>
       <div className="not-found-container">
         <h1>Page Not Found</h1>
+
         <p>Sorry, we couldn't find the page you're looking for. It might have been moved or doesn't exist.</p>
         <p>If you think there's a mistake or issue on our behalf, please let us know
           <Link href="/info#contact-us"> here</Link>.</p>
