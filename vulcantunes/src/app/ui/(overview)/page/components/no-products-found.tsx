6750af5bc3929f4c8ef0e6098c8b07bb2202cc5e
lucas <lucaseschultz@gmail.com>
2025-04-29 12:33:06 -0600

    Added link to "Contact Us" form to avoid site abandonment/improve user experience

--- vulcantunes/src/app/ui/(overview)/page/components/no-products-found.tsx
+++ vulcantunes/src/app/ui/(overview)/page/components/no-products-found.tsx
@@ -1,10 +1,13 @@
 import {memo} from 'react'
+import Link from 'next/link'
 
 export const NoProductsFound = memo(function NoProductsFound() {
   return (
     <div className="no-products">
       <h2>No Products Found</h2>
       <p>Try adjusting your search or filter criteria</p>
+      <p>If you think there's a mistake or issue on our behalf, please let us know
+        <Link href="/info#contact-us">here</Link></p>
     </div>
   )
 })
