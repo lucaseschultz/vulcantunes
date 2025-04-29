6155dcfa7e7b13394ec4eb48baaebbb84d3d23b0
lucas <lucaseschultz@gmail.com>
2025-04-29 14:25:05 -0600

    Added space between words (link) and added period at end of sentence

--- vulcantunes/src/app/ui/(overview)/page/components/no-products-found.tsx
+++ vulcantunes/src/app/ui/(overview)/page/components/no-products-found.tsx
@@ -7,7 +7,7 @@ export const NoProductsFound = memo(function NoProductsFound() {
       <h2>No Products Found</h2>
       <p>Try adjusting your search or filter criteria</p>
       <p>If you think there's a mistake or issue on our behalf, please let us know
-        <Link href="/info#contact-us">here</Link></p>
+        <Link href="/info#contact-us"> here</Link>.</p>
     </div>
   )
 })
