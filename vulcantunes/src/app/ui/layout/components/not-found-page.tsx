0cd4997a8ab528f056c10c74f7c388699d8b302d
lucas <lucaseschultz@gmail.com>
2025-04-29 14:53:24 -0600

    Created .return-home-button and replaced .return-home-link with it

--- vulcantunes/src/app/ui/layout/components/not-found-page.tsx
+++ vulcantunes/src/app/ui/layout/components/not-found-page.tsx
@@ -15,7 +15,7 @@ export default function NotFoundPage() {
         <p>If you think there's a mistake or issue on our behalf, please let us know
           <Link href="/info#contact-us"> here</Link>.</p>
 
-        <div className="return-home-link"><Link href="/">Return Home</Link></div>
+        <div className="return-home-button"><Link href="/">Return Home</Link></div>
       </div>
     </div>
   );
