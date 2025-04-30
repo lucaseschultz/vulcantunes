f5bb60d32a540f16b3c7262530cac75163449065
lucas <lucaseschultz@gmail.com>
2025-04-30 13:19:33 -0600

    Made wish-list client, for to ProductsErrorBoundary

--- vulcantunes/src/app/(overview)/wish-list/page.tsx
+++ vulcantunes/src/app/(overview)/wish-list/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import '@/src/app/ui/(overview)/wish-list/styles/wish-list.css'
 import WishListContent from '@/src/app/ui/(overview)/wish-list/components/wish-list-content'
 import {ProductsErrorBoundary} from "@/src/app/ui/(overview)/layout/components/products-error-boundary";
