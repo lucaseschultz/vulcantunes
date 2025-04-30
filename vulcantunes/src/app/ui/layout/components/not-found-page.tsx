7a1a6edadac1567e08a0d6205a3bf4a2cde30508
lucas <lucaseschultz@gmail.com>
2025-04-30 13:26:17 -0600

    Renamed not-found.scss to be page-not-found.scss for clearer naming

--- vulcantunes/src/app/ui/layout/components/not-found-page.tsx
+++ vulcantunes/src/app/ui/layout/components/not-found-page.tsx
@@ -2,7 +2,7 @@
 
 import Link from 'next/link';
 import HeaderLogo from "@/src/app/ui/layout/components/header-logo";
-import '../styles/not-found.css';
+import '../styles/page-not-found.css';
 
 export default function NotFoundPage() {
   return (
