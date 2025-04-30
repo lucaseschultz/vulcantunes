1505ee91e3fec6b7cd60697cbf8e4a5bba2ace47
lucas <lucaseschultz@gmail.com>
2025-04-30 09:11:01 -0600

    Added wishlist page content

--- vulcantunes/src/app/(overview)/wish-list/page.tsx
+++ vulcantunes/src/app/(overview)/wish-list/page.tsx
@@ -1,7 +1,11 @@
 import '@/src/app/ui/(overview)/wish-list/styles/wish-list.css'
+import WishListContent from '@/src/app/ui/(overview)/wish-list/components/wish-list-content'
 
 export default function Page() {
   return (
-    <div></div>
+    <div className="wish-list-container">
+      <h1>Your Wish List</h1>
+      <WishListContent/>
+    </div>
   );
 };
