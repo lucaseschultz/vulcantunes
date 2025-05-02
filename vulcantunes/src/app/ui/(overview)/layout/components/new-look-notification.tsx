1a4416457aae2e23ed0971076e140dca33190777
lucas <lucaseschultz@gmail.com>
2025-05-02 12:32:15 -0600

    Corrected whitespace

--- vulcantunes/src/app/ui/(overview)/layout/components/new-look-notification.tsx
+++ vulcantunes/src/app/ui/(overview)/layout/components/new-look-notification.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import { X } from '@phosphor-icons/react'
-import { useState, useEffect } from 'react'
+import {X} from '@phosphor-icons/react'
+import {useState, useEffect} from 'react'
 
 export default function NewLookNotification() {
   const [isVisible, setIsVisible] = useState(false)
@@ -24,7 +24,8 @@ export default function NewLookNotification() {
         <div className="new-look-text">
           <h3>Welcome to our new look! 🎵</h3>
           <p>
-            We've refreshed our design to serve you better, but we're still the same VulcanTunes you know and trust. Enjoy the new experience!
+            We've refreshed our design to serve you better, but we're still the same VulcanTunes you know and trust.
+            Enjoy the new experience!
           </p>
         </div>
         <button
@@ -32,7 +33,7 @@ export default function NewLookNotification() {
           className="new-look-close"
           aria-label="Close notification"
         >
-          <X size={24} />
+          <X size={24}/>
         </button>
       </div>
     </div>
